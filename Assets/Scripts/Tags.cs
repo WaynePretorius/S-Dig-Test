1ce2bfbae15b00c90f6271d0369928402e65d032
Wayne-PC\Wayne <wayne007pretorius@gmail.com>
2021-12-30 22:37:36 +0200

    Initital Push

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,9 @@
+//Static class for all string referneces
+public  static class Tags
+{
+
+    //movement Tags
+    public static string MOVE_HORIZON = "";
+    public static string MOVE_VERTI = "";
+
+}
